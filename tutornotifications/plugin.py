openedx/tutor-contrib-notifications

feat: Initial implementation
Add support for installing the frontend notification plugin and toggling the
waffle flag.

tutornotifications/plugin.py
@@ -1,9 +1,9 @@
 import os
 from glob import glob
 
-import click
 import importlib_resources
 from tutor import hooks
+from tutormfe.hooks import PLUGIN_SLOTS
 
 from .__about__ import __version__
 
@@ -45,21 +45,10 @@ hooks.Filters.CONFIG_OVERRIDES.add_items(
 # INITIALIZATION TASKS
 ########################################
 
-# To add a custom initialization task, create a bash script template under:
-# tutornotifications/templates/notifications/tasks/
-# and then add it to the MY_INIT_TASKS list. Each task is in the format:
-# ("<service>", ("<path>", "<to>", "<script>", "<template>"))
 MY_INIT_TASKS: list[tuple[str, tuple[str, ...]]] = [
-    # For example, to add LMS initialization steps, you could add the script template at:
-    # tutornotifications/templates/notifications/tasks/lms/init.sh
-    # And then add the line:
-    ### ("lms", ("notifications", "tasks", "lms", "init.sh")),
+    ("lms", ("notifications", "tasks", "lms", "waffle_flags")),
 ]
 
-
-# For each task added to MY_INIT_TASKS, we load the task template
-# and add it to the CLI_DO_INIT_TASKS filter, which tells Tutor to
-# run it as part of the `init` job.
 for service, template_path in MY_INIT_TASKS:
     full_path: str = str(
         importlib_resources.files("tutornotifications")
@@ -68,57 +57,6 @@ for service, template_path in MY_INIT_TASKS:
     with open(full_path, encoding="utf-8") as init_task_file:
         init_task: str = init_task_file.read()
     hooks.Filters.CLI_DO_INIT_TASKS.add_item((service, init_task))
-
-
-########################################
-# DOCKER IMAGE MANAGEMENT
-########################################
-
-
-# Images to be built by `tutor images build`.
-# Each item is a quadruple in the form:
-#     ("<tutor_image_name>", ("path", "to", "build", "dir"), "<docker_image_tag>", "<build_args>")
-hooks.Filters.IMAGES_BUILD.add_items(
-    [
-        # To build `myimage` with `tutor images build myimage`,
-        # you would add a Dockerfile to templates/notifications/build/myimage,
-        # and then write:
-        ### (
-        ###     "myimage",
-        ###     ("plugins", "notifications", "build", "myimage"),
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ###     (),
-        ### ),
-    ]
-)
-
-
-# Images to be pulled as part of `tutor images pull`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PULL.add_items(
-    [
-        # To pull `myimage` with `tutor images pull myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ### ),
-    ]
-)
-
-
-# Images to be pushed as part of `tutor images push`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PUSH.add_items(
-    [
-        # To push `myimage` with `tutor images push myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ### ),
-    ]
-)
 
 
 ########################################
@@ -155,73 +93,34 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 
 # For each file in tutornotifications/patches,
 # apply a patch based on the file's name and contents.
-for path in glob(str(importlib_resources.files("tutornotifications") / "patches" / "*")):
+for path in glob(
+    str(importlib_resources.files("tutornotifications") / "patches" / "*")
+):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
 
 ########################################
-# CUSTOM JOBS (a.k.a. "do-commands")
+# MFE PLUGIN SLOTS
 ########################################
 
-# A job is a set of tasks, each of which run inside a certain container.
-# Jobs are invoked using the `do` command, for example: `tutor local do importdemocourse`.
-# A few jobs are built in to Tutor, such as `init` and `createuser`.
-# You can also add your own custom jobs:
+notification_drawer_config = """
+    {
+      op: PLUGIN_OPERATIONS.Insert,
+      widget: {
+        id: 'notification-drawer-widget',
+        type: DIRECT_PLUGIN,
+        RenderWidget: NotificationsTray,
+      },
+    }
+"""
 
-
-# To add a custom job, define a Click command that returns a list of tasks,
-# where each task is a pair in the form ("<service>", "<shell_command>").
-# For example:
-### @click.command()
-### @click.option("-n", "--name", default="plugin developer")
-### def say_hi(name: str) -> list[tuple[str, str]]:
-###     """
-###     An example job that just prints 'hello' from within both LMS and CMS.
-###     """
-###     return [
-###         ("lms", f"echo 'Hello from LMS, {name}!'"),
-###         ("cms", f"echo 'Hello from CMS, {name}!'"),
-###     ]
-
-
-# Then, add the command function to CLI_DO_COMMANDS:
-## hooks.Filters.CLI_DO_COMMANDS.add_item(say_hi)
-
-# Now, you can run your job like this:
-#   $ tutor local do say-hi --name="Ty Hob"
-
-
-#######################################
-# CUSTOM CLI COMMANDS
-#######################################
-
-# Your plugin can also add custom commands directly to the Tutor CLI.
-# These commands are run directly on the user's host computer
-# (unlike jobs, which are run in containers).
-
-# To define a command group for your plugin, you would define a Click
-# group and then add it to CLI_COMMANDS:
-
-
-### @click.group()
-### def notifications() -> None:
-###     pass
-
-
-### hooks.Filters.CLI_COMMANDS.add_item(notifications)
-
-
-# Then, you would add subcommands directly to the Click group, for example:
-
-
-### @notifications.command()
-### def example_command() -> None:
-###     """
-###     This is helptext for an example command.
-###     """
-###     print("You've run an example command.")
-
-
-# This would allow you to run:
-#   $ tutor notifications example-command
+PLUGIN_SLOTS.add_items(
+    [
+        (
+            "all",
+            "org.openedx.frontend.layout.header_desktop_secondary_menu.v1",
+            notification_drawer_config,
+        ),
+    ]
+)

openedx/tutor-contrib-platform-notifications

Co-authored-by: Farhaan Bukhsh <farhaan@opencraft.com>

tutornotifications/plugin.py
@@ -1,9 +1,9 @@
 import os
 from glob import glob
 
-import click
 import importlib_resources
 from tutor import hooks
+from tutormfe.hooks import PLUGIN_SLOTS
 
 from .__about__ import __version__
 
@@ -45,21 +45,10 @@ hooks.Filters.CONFIG_OVERRIDES.add_items(
 # INITIALIZATION TASKS
 ########################################
 
-# To add a custom initialization task, create a bash script template under:
-# tutornotifications/templates/notifications/tasks/
-# and then add it to the MY_INIT_TASKS list. Each task is in the format:
-# ("<service>", ("<path>", "<to>", "<script>", "<template>"))
 MY_INIT_TASKS: list[tuple[str, tuple[str, ...]]] = [
-    # For example, to add LMS initialization steps, you could add the script template at:
-    # tutornotifications/templates/notifications/tasks/lms/init.sh
-    # And then add the line:
-    ### ("lms", ("notifications", "tasks", "lms", "init.sh")),
+    ("lms", ("notifications", "tasks", "lms", "waffle_flags")),
 ]
 
-
-# For each task added to MY_INIT_TASKS, we load the task template
-# and add it to the CLI_DO_INIT_TASKS filter, which tells Tutor to
-# run it as part of the `init` job.
 for service, template_path in MY_INIT_TASKS:
     full_path: str = str(
         importlib_resources.files("tutornotifications")
@@ -68,57 +57,6 @@ for service, template_path in MY_INIT_TASKS:
     with open(full_path, encoding="utf-8") as init_task_file:
         init_task: str = init_task_file.read()
     hooks.Filters.CLI_DO_INIT_TASKS.add_item((service, init_task))
-
-
-########################################
-# DOCKER IMAGE MANAGEMENT
-########################################
-
-
-# Images to be built by `tutor images build`.
-# Each item is a quadruple in the form:
-#     ("<tutor_image_name>", ("path", "to", "build", "dir"), "<docker_image_tag>", "<build_args>")
-hooks.Filters.IMAGES_BUILD.add_items(
-    [
-        # To build `myimage` with `tutor images build myimage`,
-        # you would add a Dockerfile to templates/notifications/build/myimage,
-        # and then write:
-        ### (
-        ###     "myimage",
-        ###     ("plugins", "notifications", "build", "myimage"),
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ###     (),
-        ### ),
-    ]
-)
-
-
-# Images to be pulled as part of `tutor images pull`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PULL.add_items(
-    [
-        # To pull `myimage` with `tutor images pull myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ### ),
-    ]
-)
-
-
-# Images to be pushed as part of `tutor images push`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PUSH.add_items(
-    [
-        # To push `myimage` with `tutor images push myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ NOTIFICATIONS_VERSION }}",
-        ### ),
-    ]
-)
 
 
 ########################################
@@ -155,73 +93,49 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 
 # For each file in tutornotifications/patches,
 # apply a patch based on the file's name and contents.
-for path in glob(str(importlib_resources.files("tutornotifications") / "patches" / "*")):
+for path in glob(
+    str(importlib_resources.files("tutornotifications") / "patches" / "*")
+):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
 
 ########################################
-# CUSTOM JOBS (a.k.a. "do-commands")
+# MFE PLUGIN SLOTS
 ########################################
 
-# A job is a set of tasks, each of which run inside a certain container.
-# Jobs are invoked using the `do` command, for example: `tutor local do importdemocourse`.
-# A few jobs are built in to Tutor, such as `init` and `createuser`.
-# You can also add your own custom jobs:
+notification_drawer_config = """
+    {
+      op: PLUGIN_OPERATIONS.Insert,
+      widget: {
+        id: 'notification-drawer-widget',
+        type: DIRECT_PLUGIN,
+        RenderWidget: NotificationsTray,
+      },
+    }
+"""
 
-
-# To add a custom job, define a Click command that returns a list of tasks,
-# where each task is a pair in the form ("<service>", "<shell_command>").
-# For example:
-### @click.command()
-### @click.option("-n", "--name", default="plugin developer")
-### def say_hi(name: str) -> list[tuple[str, str]]:
-###     """
-###     An example job that just prints 'hello' from within both LMS and CMS.
-###     """
-###     return [
-###         ("lms", f"echo 'Hello from LMS, {name}!'"),
-###         ("cms", f"echo 'Hello from CMS, {name}!'"),
-###     ]
-
-
-# Then, add the command function to CLI_DO_COMMANDS:
-## hooks.Filters.CLI_DO_COMMANDS.add_item(say_hi)
-
-# Now, you can run your job like this:
-#   $ tutor local do say-hi --name="Ty Hob"
-
-
-#######################################
-# CUSTOM CLI COMMANDS
-#######################################
-
-# Your plugin can also add custom commands directly to the Tutor CLI.
-# These commands are run directly on the user's host computer
-# (unlike jobs, which are run in containers).
-
-# To define a command group for your plugin, you would define a Click
-# group and then add it to CLI_COMMANDS:
-
-
-### @click.group()
-### def notifications() -> None:
-###     pass
-
-
-### hooks.Filters.CLI_COMMANDS.add_item(notifications)
-
-
-# Then, you would add subcommands directly to the Click group, for example:
-
-
-### @notifications.command()
-### def example_command() -> None:
-###     """
-###     This is helptext for an example command.
-###     """
-###     print("You've run an example command.")
-
-
-# This would allow you to run:
-#   $ tutor notifications example-command
+PLUGIN_SLOTS.add_items(
+    [
+        (
+            "all",
+            "org.openedx.frontend.layout.desktop_header_item_slot.v1",
+            notification_drawer_config,
+        ),
+        (
+            "all",
+            "org.openedx.frontend.layout.learning_header_item_slot.v1",
+            notification_drawer_config,
+        ),
+        (
+            "all",
+            "org.openedx.frontend.layout.mobile_header_item_slot.v1",
+            notification_drawer_config,
+        ),
+        (
+            "all",
+            "org.openedx.frontend.layout.studio_header_item_slot.v1",
+            notification_drawer_config,
+        ),
+    ]
+)